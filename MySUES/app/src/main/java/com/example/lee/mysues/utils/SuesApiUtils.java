a91ec8d24bcacff40387ae90b6747ca62aa17ce3
lixu <859518963@qq.com>
2015-01-23 17:53:30 +0800

    修正新闻url，获得正确的总页数

--- MySUES/app/src/main/java/com/example/lee/mysues/utils/SuesApiUtils.java
+++ MySUES/app/src/main/java/com/example/lee/mysues/utils/SuesApiUtils.java
@@ -21,7 +21,7 @@ public class SuesApiUtils {
     public static final String NEWS_URL_MTJJ = "http://news.sues.edu.cn/s/593/t/301/p/34/list.htm";
     public static final String NEWS_URL_BMXW = "http://news.sues.edu.cn/s/593/t/301/p/35/list.htm";
 
-
+    //用于拼接的基础url
     private static final String NEWS_URL_XXYW_PART = "http://news.sues.edu.cn/s/593/t/301/p/31/i/";
     private static final String NEWS_URL_XYKX_PART = "http://news.sues.edu.cn/s/593/t/301/p/32/i/";
     private static final String NEWS_URL_KJDT_PART = "http://news.sues.edu.cn/s/593/t/301/p/33/i/";
@@ -29,7 +29,13 @@ public class SuesApiUtils {
     private static final String NEWS_URL_BMXW_PART = "http://news.sues.edu.cn/s/593/t/301/p/35/i/";
 
     private static final String URL_END = "/list.htm";
+    private static final String TOTAL_PAGE_END = "/i.htm";
 
+    //URL规则：
+    //基础url + 当前页码 + URL_END = 对应页码的新闻页面url
+    //基础url + TOTAL_PAGE_END = 该页面的新闻总条数
+    
+    
     private static int getTotalPageByUrl(String url){
         String htmlStr = null;
         try {
@@ -38,10 +44,12 @@ public class SuesApiUtils {
             e.printStackTrace();
         }
 
-        Document doc = Jsoup.parse(htmlStr);
-        Element element = doc.getElementById("LIST_PAGINATION_COUNT");
+        //Document doc = Jsoup.parse(htmlStr);
+        //Element element = doc.getElementById("LIST_PAGINATION_COUNT");
 
-        return Integer.parseInt(element.text());
+        int totalNewsItem = Integer.parseInt(htmlStr);  //新闻总条数
+        int totalPage = totalNewsItem/21 + totalNewsItem%21==0?0:1; //新闻总页数
+        return totalPage;
     }
 
     /**
@@ -53,19 +61,19 @@ public class SuesApiUtils {
         int totalPage = 0;
         switch(newsType){
             case NewsTypes.NEWS_TPYE_XXYW:
-                totalPage = getTotalPageByUrl(NEWS_URL_XXYW);
+                totalPage = getTotalPageByUrl(NEWS_URL_XXYW_PART+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_XYKX:
-                totalPage = getTotalPageByUrl(NEWS_URL_XYKX);
+                totalPage = getTotalPageByUrl(NEWS_URL_XYKX+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_KJDT:
-                totalPage = getTotalPageByUrl(NEWS_URL_KJDT);
+                totalPage = getTotalPageByUrl(NEWS_URL_KJDT+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_MTJJ:
-                totalPage = getTotalPageByUrl(NEWS_URL_MTJJ);
+                totalPage = getTotalPageByUrl(NEWS_URL_MTJJ+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_BMXW:
-                totalPage = getTotalPageByUrl(NEWS_URL_BMXW);
+                totalPage = getTotalPageByUrl(NEWS_URL_BMXW+TOTAL_PAGE_END);
                 break;
             default:
                 break;
