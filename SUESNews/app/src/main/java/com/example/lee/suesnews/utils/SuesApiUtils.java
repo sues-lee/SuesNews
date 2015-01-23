2b60a4f09a88ce463874f5228dabf19d1eff026a
lixu <lixu9527@gmail.com>
2015-01-24 00:31:46 +0800

    完成新闻列表数据抓取

--- SUESNews/app/src/main/java/com/example/lee/suesnews/utils/SuesApiUtils.java
+++ SUESNews/app/src/main/java/com/example/lee/suesnews/utils/SuesApiUtils.java
@@ -1,11 +1,13 @@
 package com.example.lee.suesnews.utils;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import com.example.lee.suesnews.common.NewsTypes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 相关API
@@ -21,15 +23,25 @@ public class SuesApiUtils {
     public static final String NEWS_URL_MTJJ = "http://news.sues.edu.cn/s/593/t/301/p/34/list.htm";
     public static final String NEWS_URL_BMXW = "http://news.sues.edu.cn/s/593/t/301/p/35/list.htm";
 
-
-    private static final String NEWS_URL_XXYW_PART = "http://news.sues.edu.cn/s/593/t/301/p/31/i/";
-    private static final String NEWS_URL_XYKX_PART = "http://news.sues.edu.cn/s/593/t/301/p/32/i/";
-    private static final String NEWS_URL_KJDT_PART = "http://news.sues.edu.cn/s/593/t/301/p/33/i/";
-    private static final String NEWS_URL_MTJJ_PART = "http://news.sues.edu.cn/s/593/t/301/p/34/i/";
-    private static final String NEWS_URL_BMXW_PART = "http://news.sues.edu.cn/s/593/t/301/p/35/i/";
+    //用于拼接的基础url
+    private static final String NEWS_URL_XXYW_PART = "http://news.sues.edu.cn/s/593/t/301/p/31/i";
+    private static final String NEWS_URL_XYKX_PART = "http://news.sues.edu.cn/s/593/t/301/p/32/i";
+    private static final String NEWS_URL_KJDT_PART = "http://news.sues.edu.cn/s/593/t/301/p/33/i";
+    private static final String NEWS_URL_MTJJ_PART = "http://news.sues.edu.cn/s/593/t/301/p/34/i";
+    private static final String NEWS_URL_BMXW_PART = "http://news.sues.edu.cn/s/593/t/301/p/35/i";
 
     private static final String URL_END = "/list.htm";
+    private static final String TOTAL_PAGE_END = ".htm";
 
+    //URL规则：
+    //基础url + "/" + 当前页码 + URL_END = 对应页码的新闻页面url
+    //基础url + TOTAL_PAGE_END = 该页面的新闻总条数
+
+    /**
+     * 根据url得到总页数
+     * @param url
+     * @return
+     */
     private static int getTotalPageByUrl(String url){
         String htmlStr = null;
         try {
@@ -37,11 +49,21 @@ public class SuesApiUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        int totalNewsItem = getIntFromString(htmlStr);  //新闻总条数
+        int totalPage = totalNewsItem/21 + (totalNewsItem%21==0?0:1); //新闻总页数
+        return totalPage;
+    }
 
-        Document doc = Jsoup.parse(htmlStr);
-        Element element = doc.getElementById("LIST_PAGINATION_COUNT");
-
-        return Integer.parseInt(element.text());
+    /**
+     * 利用正则表达式提取字符串中的数字
+     * @param str 被提取的字符串
+     * @return 提取的数字
+     */
+    private static int getIntFromString(String str){
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+        return Integer.parseInt(m.replaceAll("").trim());
     }
 
     /**
@@ -52,20 +74,20 @@ public class SuesApiUtils {
     public static int getTotalPage(int newsType){
         int totalPage = 0;
         switch(newsType){
-            case NewsTypes.NEWS_TPYE_XXYW:
-                totalPage = getTotalPageByUrl(NEWS_URL_XXYW);
+             case NewsTypes.NEWS_TPYE_XXYW:
+                totalPage = getTotalPageByUrl(NEWS_URL_XXYW_PART+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_XYKX:
-                totalPage = getTotalPageByUrl(NEWS_URL_XYKX);
+                totalPage = getTotalPageByUrl(NEWS_URL_XYKX_PART+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_KJDT:
-                totalPage = getTotalPageByUrl(NEWS_URL_KJDT);
+                totalPage = getTotalPageByUrl(NEWS_URL_KJDT_PART+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_MTJJ:
-                totalPage = getTotalPageByUrl(NEWS_URL_MTJJ);
+                totalPage = getTotalPageByUrl(NEWS_URL_MTJJ_PART+TOTAL_PAGE_END);
                 break;
             case NewsTypes.NEWS_TPYE_BMXW:
-                totalPage = getTotalPageByUrl(NEWS_URL_BMXW);
+                totalPage = getTotalPageByUrl(NEWS_URL_BMXW_PART+TOTAL_PAGE_END);
                 break;
             default:
                 break;
@@ -87,19 +109,19 @@ public class SuesApiUtils {
         String url = null;
         switch(newsType){
             case NewsTypes.NEWS_TPYE_XXYW:
-                url = NEWS_URL_XXYW_PART + currentPageString;
+                url = NEWS_URL_XXYW_PART + "/" + currentPageString;
                 break;
             case NewsTypes.NEWS_TPYE_XYKX:
-                url = NEWS_URL_XYKX_PART + currentPageString;
+                url = NEWS_URL_XYKX_PART + "/" + currentPageString;
                 break;
             case NewsTypes.NEWS_TPYE_KJDT:
-                url = NEWS_URL_KJDT_PART + currentPageString;
+                url = NEWS_URL_KJDT_PART + "/" + currentPageString;
                 break;
             case NewsTypes.NEWS_TPYE_MTJJ:
-                url = NEWS_URL_MTJJ_PART + currentPageString;
+                url = NEWS_URL_MTJJ_PART + "/" + currentPageString;
                 break;
             case NewsTypes.NEWS_TPYE_BMXW:
-                url = NEWS_URL_BMXW_PART + currentPageString;
+                url = NEWS_URL_BMXW_PART + "/" + currentPageString;
                 break;
             default:
                 break;
